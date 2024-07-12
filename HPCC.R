9590c163fdeeb607109db617f26be53cbde5908f
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2024-07-12 11:32:20 -0400

    Initial commit

---
+++ HPCC.R
@@ -0,0 +1,19 @@
+# HPCC
+
+# 3 terminals:
+
+## 1 local computer: access github, upload files to Ydrive
+
+# Documents/GitHub/REPO
+# git config --global user.email 'amaral.bruuna@gmail.com'
+
+## 2 HPCC: submit job (run the analysis)
+
+## 3 drive: access hpcc from your computer: push files from here (download ready github files)
+
+# cd .. # hundred times!
+
+# cd Volumes/home-207/bamaral/
+
+## sb save as notepad 
+
---
+++ draft_script.R
@@ -0,0 +1,57 @@
+#? *********************************************************************************
+#? -------------------------------   Amazing Title   -------------------------------
+#? *********************************************************************************
+#
+#! Code to ...
+#
+#! Source ---------------------------------------------
+#           - :
+#           - :
+#
+#! Input ----------------------------------------------
+#           - :
+#           - :
+#
+#! Output ----------------------------------------------
+#           - :
+#           - :
+
+# Print script file name
+context <- rstudioapi::getSourceEditorContext()
+cat("\n", "\n", "\n", 'Current script: ', basename(contextpath), "\n", "\n", "\n", "\n")
+
+#! Package library and versions -------------------------
+#  Created a library repo?
+#  (  )yes  (  )no
+#  renv::init()
+
+# Load an existing library?
+#  renv::restore()
+
+# Installed new packages?
+#  renv::snapshot()
+
+# detach packages and clear workspace
+if(!require(freshr)){install.packages('freshr')}
+freshr::freshr()
+
+#! Load packages ---------------------------------------
+library(conflicted)
+library(tidyverse)
+library(glue)
+
+conflicts_prefer(dplyr::select)
+conflicts_prefer(dplyr::filter)
+# conflicts_prefer(scales::alpha)
+
+#! Make functions --------------------------------------
+colanmes <- colnames
+lenght <- length
+`%!in%` <- Negate(`%in%`)
+
+#! Source code -----------------------------------------
+
+#! Import data -----------------------------------------
+## file paths
+
+## read files
